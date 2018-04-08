3a7bd4618fd2c5dce95e1395f9be767af118462f
akashmaharana <akashmaharana93@gmail.com>
2018-04-08 10:16:33 +0530

    Hibernate_IdClass_CompositeKey

---
+++ Hibernate_IdClass_CompositeKey/src/com/akash/maharana/bean/Certification.java
@@ -0,0 +1,27 @@
+package com.akash.maharana.bean;
+
+public class Certification {
+	
+	private String certificationName;
+	private String certifictionCode;
+	private String certifictionType;
+	public String getCertificationName() {
+		return certificationName;
+	}
+	public void setCertificationName(String certificationName) {
+		this.certificationName = certificationName;
+	}
+	public String getCertifictionCode() {
+		return certifictionCode;
+	}
+	public void setCertifictionCode(String certifictionCode) {
+		this.certifictionCode = certifictionCode;
+	}
+	public String getCertifictionType() {
+		return certifictionType;
+	}
+	public void setCertifictionType(String certifictionType) {
+		this.certifictionType = certifictionType;
+	}
+
+}
